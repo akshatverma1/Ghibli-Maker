7c3b56cf6ba53f70443172528c891f0147047772
akshatverma1 <akshaverma14@gmail.com>
2025-04-03 23:23:31 +0530

    Don

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,15 @@
 const express = require ("express");
+require('dotenv').config()
+
 
 const app = express();
-const PORT = 3009;
-app.listen(PORT,()=>{
-    console.log("Port is Running on = "+PORT);
+const POR = process.env.PORTs;
+console.log(POR);
+
+
+
+app.listen(POR,()=>{
+    console.log("Port is Running on = "+POR);
 })
 
 app.get("/",(req,res)=>{
